42bedb85d2a459dccc037321caa648f0dd10e085
gitogito186 <cap168218@gmail.com>
2025-08-14 02:03:11 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+
+{
+  "name": "family888-bot",
+  "version": "1.0.0",
+  "description": "Family 888 WhatsApp Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@open-wa/wa-automate": "^4.35.0",
+    "fs-extra": "^10.0.0"
+  }
+}
