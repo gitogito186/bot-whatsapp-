0363dbbabd701af8460ded65c12a0a8fb13b8447
gitogito186 <cap168218@gmail.com>
2025-08-14 02:17:41 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,23 @@
+
+{
+  "users": {
+    "6281234567890": {
+      "premium": false,
+      "name": "User1",
+      "registeredAt": "2025-08-13"
+    },
+    "6289876543210": {
+      "premium": true,
+      "name": "User2",
+      "registeredAt": "2025-08-01"
+    }
+  },
+  "owners": ["6281111111111"],
+  "premiumUsers": ["6289876543210"],
+  "groups": {
+    "1203630xxxxxx@g.us": {
+      "welcome": true,
+      "antiLink": false
+    }
+  }
+}
